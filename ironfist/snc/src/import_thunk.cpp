c03ae5daaee0053ce1634ed318a4781231ce0ddc
James Koppel <devnull@localhost>
2012-08-04 16:08:01 -0700

    Some cleanup

--- ironfist/snc/src/import_thunk.cpp
+++
@@ -1,6 +0,0 @@
-extern "C" {
-
-	void hero::AddSpell(hero * this, int  spell, int  knowledge);
-	char hero::Stats(hero * this, PRIMARY_SKILL  skill);
-	signed int hero::HasArtifact(hero * this, ARTIFACT  artifact);
-}
---
+++ ironfist/src/cpp/compat.cpp
@@ -0,0 +1,22 @@
+
+#ifndef COMPAT_H
+#define COMPAT_H
+
+#include "import_thunk.cpp"
+
+
+extern "C" void* __cdecl op_new(unsigned int x) {
+       return operator new(x);
+}
+
+extern "C" void __cdecl op_del(void* x) {
+       operator delete(x);
+}
+
+
+int main() {
+       _start();
+       return 0;
+}
+
+#endif
