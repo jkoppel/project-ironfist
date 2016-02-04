5270d43e78d97abf8e0f5ccb40abf7d103d76821
rems75 <rtachet@mit.edu>
2016-02-03 20:25:38 -0500

    Loading and Saving MapVariables

--- ironfist/src/cpp/scripting/scripting.h
+++ ironfist/src/cpp/scripting/scripting.h
@@ -1,11 +1,15 @@
 #ifndef SCRIPTING_H
 #define SCRIPTING_H
 
-void DisplayError();
 void ScriptingInit(char*);
 void ScriptingInitFromString(const char*);
 void ScriptingShutdown();
 
 char *GetScriptContents();
+
+void DisplayError();
+
+void GetNextMapVariable(int &, const char *&, const char *&);
+void SetMapVariables(const char*, const char*);
 
 #endif
