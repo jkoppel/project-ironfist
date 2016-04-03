0d262ac7313a46affb98211c9cd8f8d3f9c07675
chrisbigart <chrisbigart@gmail.com>
2016-04-03 18:22:21 -0400

    prefs

--- ironfist/src/cpp/game/event_loop.cpp
+++ ironfist/src/cpp/game/event_loop.cpp
@@ -1,6 +1,6 @@
 #include "base.h"
 #include "game/game.h"
-#include "registry_prefs.h"
+#include "prefs.h"
 
 //adding a call to Sleep() during event processing will reduce excessive cpu usage
 //on my machine, 10ms seems to reduce cpu load to <1% while not having any effect
@@ -11,7 +11,7 @@ const int MAX_SLEEP_VALUE = 1000; //sanity check
 void __fastcall Process1WindowsMessage_orig();
 void __fastcall Process1WindowsMessage()
 	{
-	static int SLEEP_DURATION = read_registry_pref<DWORD>("Sleep Event Loop");
+	static int SLEEP_DURATION = read_pref<DWORD>("Sleep Event Loop");
 
 	if(SLEEP_DURATION >= 0 && SLEEP_DURATION <= MAX_SLEEP_VALUE)
 		Sleep(SLEEP_DURATION);
--- ironfist/src/cpp/registry_prefs.h
+++ ironfist/src/cpp/registry_prefs.h
@@ -6,14 +6,14 @@
 
 template<typename T> T read_registry_pref(const std::string& pref_key);
 
-template<> DWORD read_registry_pref(const std::string& pref_key)
-	{
+LPCSTR Heroes2RegistrySubKey = "SOFTWARE\\New World Computing\\Heroes of Might and Magic 2\\1.0";
+
+template<> DWORD read_registry_pref(const std::string& pref_key) {
 	DWORD Type;
 	DWORD cbData = 4;
 	DWORD Data = (DWORD)-1;
-	LPCSTR SubKey = "SOFTWARE\\New World Computing\\Heroes of Might and Magic 2\\1.0";
-	HKEY hKey = nullptr;
-	if(RegCreateKeyA(HKEY_LOCAL_MACHINE, SubKey, &hKey) == ERROR_SUCCESS)
+	HKEY hKey = 0;
+	if(RegCreateKeyA(HKEY_LOCAL_MACHINE, Heroes2RegistrySubKey, &hKey) == ERROR_SUCCESS)
 		{
 		RegQueryValueExA(hKey, pref_key.c_str(), 0, &Type, (LPBYTE)&Data, &cbData);
 		}
@@ -23,13 +23,11 @@ template<> DWORD read_registry_pref(const std::string& pref_key)
 
 template<typename T> bool write_registry_pref(const std::string& pref_key, const T& value);
 
-template<> bool write_registry_pref(const std::string& pref_key, const DWORD& value)
-	{
+template<> bool write_registry_pref(const std::string& pref_key, const DWORD& value) {
 	DWORD cbData = 4;
-	LPCSTR SubKey = "SOFTWARE\\New World Computing\\Heroes of Might and Magic 2\\1.0";
-	HKEY hKey = nullptr;
+	HKEY hKey = 0;
 
-	if(RegOpenKeyExA(HKEY_LOCAL_MACHINE, SubKey, 0, 0xF003Fu, &hKey) != ERROR_SUCCESS)
+	if(RegOpenKeyExA(HKEY_LOCAL_MACHINE, Heroes2RegistrySubKey, 0, KEY_ALL_ACCESS, &hKey) != ERROR_SUCCESS)
 		return false;
 
 	if(RegSetValueExA(hKey, pref_key.c_str(), 0, REG_DWORD, (LPBYTE)&value, cbData) != ERROR_SUCCESS)
