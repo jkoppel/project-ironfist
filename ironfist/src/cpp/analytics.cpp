b4e9f6c7f56298584b47bd5e11862b9a25910fa5
rems75 <rtachet@mit.edu>
2016-05-14 01:42:14 -0400

    UUIDs from POCO

--- ironfist/src/cpp/analytics.cpp
+++ ironfist/src/cpp/analytics.cpp
@@ -3,13 +3,13 @@
 #include "Poco/Net/HTTPClientSession.h"
 #include "Poco/Net/HTTPRequest.h"
 #include "Poco/URI.h"
+#include "Poco/UUID.h"
+#include "Poco/UUIDGenerator.h"
 
 #include "analytics.h"
 #include "base.h"
 #include "prefs.h"
 
-// For UUID handling
-#pragma comment(lib, "rpcrt4.lib") 
 
 extern std::string gameAction = "gameAction";
 extern std::string mapAction = "mapAction";
@@ -17,38 +17,30 @@ extern std::string open = "open";
 
 const std::string IRONFIST_TRACKER_ID("UA-24357556-4");
 
-std::string createOrGetUuid() {
-	std::string sUuid;
-	bool getUuid = read_pref<std::string>("Uuid", sUuid);
+const std::string createOrGetUuid() {
+	std::string uuid;
+	bool getUuid = read_pref<std::string>("Uuid", uuid);
 	if (!getUuid) {
-		UUID uuid;
-		UuidCreate(&uuid);
-		unsigned char * str;
-		UuidToStringA(&uuid, &str);
-		std::string s((char*)str);
-		RpcStringFreeA(&str);
-		sUuid = s;
-		bool success = write_pref("Uuid", sUuid);
+		Poco::UUID pocoUuid = Poco::UUIDGenerator().create();
+		uuid = pocoUuid.toString();
+		bool setUuid = write_pref("Uuid", uuid);
 	}
-	return sUuid;
+	return uuid;
 }
 
 void send_event(std::string category, std::string action) {
-
-	static std::string Uuid = createOrGetUuid();
-
+	static const std::string uuid = createOrGetUuid();
 	Poco::Net::HTTPClientSession session("www.google-analytics.com");
 	Poco::Net::HTTPRequest req(Poco::Net::HTTPRequest::HTTP_POST, "/collect", Poco::Net::HTTPMessage::HTTP_1_1);
 	Poco::URI uri("");
 	uri.addQueryParameter("v", "1");
 	uri.addQueryParameter("tid", IRONFIST_TRACKER_ID);
-	uri.addQueryParameter("cid", Uuid);
+	uri.addQueryParameter("cid", uuid);
 	uri.addQueryParameter("t", "event");
 	uri.addQueryParameter("an", "Ironfist");
 	uri.addQueryParameter("ec", category);
 	uri.addQueryParameter("ea", action);
-	uri.addQueryParameter("el", Uuid);
+	uri.addQueryParameter("el", uuid);
 	req.setContentLength(uri.getRawQuery().length());
 	session.sendRequest(req) << uri.getRawQuery();
-
 }
