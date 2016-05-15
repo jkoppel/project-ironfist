dc729118c5aaf7fc68ce959608151a384f091eb0
rems75 <rtachet@mit.edu>
2016-05-15 18:53:22 -0400

    Event naming policy

--- ironfist/src/cpp/analytics.h
+++ ironfist/src/cpp/analytics.h
@@ -4,4 +4,8 @@ extern const std::string gameAction;
 extern const std::string mapAction;
 extern const std::string open;
 
+// Category and action describe the event sent to Google Analytics.
+// Both are strings, should be lower case, except for compound names,
+// for which "interior names" should start with an upper case.
+// Example: "open", "gameAction" and "mapAction".
 void send_event(const std::string &category, const std::string &action);
