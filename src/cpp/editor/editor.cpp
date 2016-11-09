78a1ee21e4cadae2ce6f19284535149ef2200f66
Sergei Nikitin <kertwaii@gmail.com>
2016-11-09 13:14:37 +0600

    Initial commit

--- src/cpp/editor/editor.cpp
+++ src/cpp/editor/editor.cpp
@@ -1,3 +1,5 @@
+#include <deque>
+
 #include "editor.h"
 
 #include "base.h"
@@ -17,4 +19,51 @@ void __fastcall ShowErrorMessage(const char *str) {
 	ShowErrorMessage_orig(buf);
 
 	FREE(buf);
+}
+
+extern fullMap gpMap;
+extern fullMap gMap2;
+
+std::deque<fullMap*> undoStack;
+
+void * __cdecl CopyMap(void) {
+	gMap2.Clone(&gpMap);
+
+	if (undoStack.size() > 10)
+	{
+		//undoStack.back().Close();
+		undoStack.pop_front();
+	}
+	fullMap *tmp = new fullMap();
+	tmp->Clone(&gMap2);
+
+	undoStack.push_back(tmp);
+	return NULL;
+}
+
+void * fullMap::Clone(fullMap *oth) {
+	if(this->cellExtras)
+		delete(this->cellExtras);
+	/*if (this->tiles)
+		delete (this->tiles);*/
+
+	this->height = oth->height;
+	this->width = oth->width;
+	// copy data to another fullMap
+	this->numCellExtras = oth->numCellExtras;
+	this->cellExtras = (mapCellExtra *)operator new(sizeof(mapCellExtra) * this->numCellExtras);
+	memcpy(this->tiles, oth->tiles, sizeof(mapCell) * this->height * this->width);
+	memcpy(this->cellExtras, oth->cellExtras, 15 * this->numCellExtras);
+
+	if (this == &gpMap)
+	{
+		if (undoStack.size() > 2)
+		{
+			// Close?
+			undoStack.pop_back();
+		}
+		if(undoStack.size())
+			gMap2 = *undoStack.back();
+	}
+	return NULL;
 }
