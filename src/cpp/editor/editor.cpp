496cad40241c28357f136b29d6647cf4319b9333
Sergei Nikitin <kertwaii@gmail.com>
2016-11-10 12:18:35 +0600

    Use stack container instead of deque

--- src/cpp/editor/editor.cpp
+++ src/cpp/editor/editor.cpp
@@ -1,4 +1,4 @@
-#include <deque>
+#include <stack>
 
 #include "editor.h"
 
@@ -23,16 +23,10 @@ void __fastcall ShowErrorMessage(const char *str) {
 
 extern fullMap gpMap;
 
-std::deque<fullMap*> undoStack;
+std::stack<fullMap*> undoStack;
 
 void * __cdecl CopyMap(void) {
-
-	if (undoStack.size() > 10) {
-		//undoStack.front().Close();
-		undoStack.pop_front();
-	}
 	fullMap *tmp = (fullMap *)operator new (sizeof(fullMap));
-	// copy data to another fullMap
 	tmp->height = gpMap.height;
 	tmp->width = gpMap.width;
 	tmp->numCellExtras = gpMap.numCellExtras;
@@ -40,7 +34,7 @@ void * __cdecl CopyMap(void) {
 	tmp->tiles = (mapCell *)operator new(sizeof(mapCell) * tmp->height * tmp->width);
 	memcpy(tmp->tiles, gpMap.tiles, sizeof(mapCell) * tmp->height * tmp->width);
 	memcpy(tmp->cellExtras, gpMap.cellExtras, 15 * tmp->numCellExtras);
-	undoStack.push_back(tmp);
+	undoStack.push(tmp);
 	return NULL;
 }
 
@@ -48,19 +42,20 @@ void * fullMap::Clone(fullMap *oth) {
 	if (undoStack.size()) {
 		delete gpMap.cellExtras;
 		delete gpMap.tiles;
-		gpMap = *undoStack.back();
-		undoStack.pop_back();
+		gpMap = *undoStack.top();
+		undoStack.pop();
 	}
 	return NULL;
 }
 
 void editManager::InitializeMap(int random, int width, int height) {
-	for (auto i : undoStack) {
+	while (undoStack.size()) {
+		fullMap *i = undoStack.top();
 		if (i->cellExtras)
 			delete i->cellExtras;
 		if (i->tiles)
 			delete i->tiles;
+		undoStack.pop();
 	}
-	undoStack.clear();
 	this->InitializeMap_orig(random, width, height);
 }
