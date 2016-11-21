5cfe5bc4a80f7b71697115603386fe9649dc6bb0
Serge Nikitin <kertwaii@gmail.com>
2016-11-21 20:43:20 +0600

    Fixed the callback name

--- src/cpp/shared/adventure/adv.cpp
+++ src/cpp/shared/adventure/adv.cpp
@@ -80,7 +80,7 @@ int advManager::Open(int idx) {
 mapCell* advManager::MoveHero(int a2, int a3, int *a4, int *a5, int *a6, int a7, int *a8, int a9){
   mapCell* res = MoveHero_orig(a2, a3, a4, a5, a6, a7, a8, a9);
   hero *hro = GetCurrentHero();
-  ScriptCallback("OnHeroMoved", hro->x, hro->y);
+  ScriptCallback("OnHeroMove", hro->x, hro->y);
   return res;
 }
 
