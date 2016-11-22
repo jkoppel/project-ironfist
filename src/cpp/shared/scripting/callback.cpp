ee3a768b2844639a4e0092642200aeb47d85ae09
Serge Nikitin <kertwaii@gmail.com>
2016-11-22 18:48:31 +0600

    Fixed templates

--- src/cpp/shared/scripting/callback.cpp
+++ src/cpp/shared/scripting/callback.cpp
@@ -12,6 +12,9 @@ void ironfist_lua_push(std::string arg) {
   lua_pushstring(map_lua, arg.c_str());
 }
 
+void ironfist_lua_pushmulti() {
+};
+
 void ScriptCallback(const char * funcName) {
   lua_getglobal(map_lua, funcName);
   lua_pcall(map_lua, 0, 0, 0);
--- src/cpp/shared/scripting/callback.h
+++ src/cpp/shared/scripting/callback.h
@@ -12,25 +12,27 @@ extern "C" {
 void ironfist_lua_push(int arg);
 void ironfist_lua_push(bool arg);
 void ironfist_lua_push(std::string arg);
+void ironfist_lua_pushmulti();
+
+template<typename T>
+void ironfist_lua_pushmulti(T first) {
+  ironfist_lua_push(first);
+}
+
+template<typename T, typename... Args>
+void ironfist_lua_pushmulti(T first, Args... args) {
+  ironfist_lua_push(first);
+  ironfist_lua_pushmulti(args...);
+}
 
 extern lua_State* map_lua;
 
-template<typename... Args, typename T>
-void ScriptCallback(const char * funcName, T first, Args... args) {
+template<typename... Args>
+void ScriptCallback(const char * funcName, Args... args) {
   lua_getglobal(map_lua, funcName);
-  ironfist_lua_push(first);
-  ironfist_lua_push(args...);
+  ironfist_lua_pushmulti(args...);
   int size = 1 + sizeof...(Args);
   lua_pcall(map_lua, size, 0, 0);
 }
-
-template<typename T>
-void ScriptCallback(const char * funcName, T first) {
-  lua_getglobal(map_lua, funcName);
-  ironfist_lua_push(first);
-  lua_pcall(map_lua, 1, 0, 0);
-}
-
-void ScriptCallback(const char * funcName);
 
 #endif
