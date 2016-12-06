3c3da65d503c26ba106c4963549915f57ee2a32e
Serge Nikitin <kertwaii@gmail.com>
2016-12-06 18:45:28 +0600

    Fixed OnMapStart and first day OnNewDay calls

--- src/cpp/shared/adventure/adv.cpp
+++ src/cpp/shared/adventure/adv.cpp
@@ -64,16 +64,6 @@ int advManager::ProcessDeSelect(tag_message *evt, int *n, mapCell **cells) {
 
 int advManager::Open(int idx) {
   int res = this->Open_orig(idx);
-  if (gpGame->day == 1 && gpGame->week == 1 && gpGame->month == 1) {
-    //The NEW_DAY event is triggered after game::PerDay, but that is not called
-    //before the first day
-    //
-    //The correct place to put this is wherever the start-of-map events fire
-    //This kinda works, but will also fire after exiting the town screen or
-    //combat on the first day
-    ScriptCallback("OnMapStart");
-    ScriptCallback("OnNewDay", gpGame->month, gpGame->week, gpGame->day);
-  }
   return res;
 }
 
