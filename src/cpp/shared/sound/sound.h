7095dbe0c41af93ad2ba2f6c467682fc0e48fd4e
Sergei Nikitin <kertwaii@gmail.com>
2016-12-23 08:54:22 +0600

    Fixed indentation and removed some outdated comments

--- src/cpp/shared/sound/sound.h
+++ src/cpp/shared/sound/sound.h
@@ -7,64 +7,52 @@ typedef void* HDIGDRIVER;
 
 #pragma pack(push,1)
 
-class soundManager : public baseManager
-	{
-	public:
-	//void *vtable;
-	//baseManager *next;
-	//baseManager *prev;
-	//int type;
-	//int idx;
-	//char name[30];
-	//int ready;
-		//virtual int Open(int);
-		//virtual void Close();
-		//virtual int Main(tag_message&);
-	public:
-		int redbookStatus;
-		HDIGDRIVER hdidriver;
-		int field_3E;
-		char _0[22];
-		void * hsamples[14];  //HSAMPLE hsamples[14];
-		int field_90;
-		int field_94;
-		int numSamplesAllocated;
-		char _1[84];
-		char _2[1152];
-		int field_570;
-		int _3;
-		int field_578;
-		char currentTrack;
-		char _4[267];
-		int aiMusicOff;
-		int volRelated;
-		int _5;
-		int field_694;
-		int field_698;
-		__int16 outputDeviceID;
-		int usingCDAudio;// int field_69E;
-		int usingRedbookRelated;
-		int field_6A6;
-		float field_6AA;
-		int someTick;
-		soundManager();
+class soundManager : public baseManager {
+public:
+  int redbookStatus;
+  HDIGDRIVER hdidriver;
+  int field_3E;
+  char _0[22];
+  void * hsamples[14];  //HSAMPLE hsamples[14];
+  int field_90;
+  int field_94;
+  int numSamplesAllocated;
+  char _1[84];
+  char _2[1152];
+  int field_570;
+  int _3;
+  int field_578;
+  char currentTrack;
+  char _4[267];
+  int aiMusicOff;
+  int volRelated;
+  int _5;
+  int field_694;
+  int field_698;
+  __int16 outputDeviceID;
+  int usingCDAudio;// int field_69E;
+  int usingRedbookRelated;
+  int field_6A6;
+  float field_6AA;
+  int someTick;
+  soundManager();
 
-		void SwitchAmbientMusic(int track);
-    struct _SAMPLE * MemorySample(sample*);
-		int CDIsPlaying();
-		int CDIsPlaying_orig();
-		void CDStartup();
-		void CDStartup_orig();
-		void CDShutdown();
-		void CDShutdown_orig();
-		void CDPlay(int track_number, signed int a3, int a4, int a5);
-		void CDStop();
-		void CDSetVolume(int, int);
-		void CDSetVolume_orig(int, int);
-		int ConvertVolume(int, int);
-		int ConvertVolume_orig(int, int);
-		void ServiceSound();
-	};
+  void SwitchAmbientMusic(int track);
+  struct _SAMPLE * MemorySample(sample*);
+  int CDIsPlaying();
+  int CDIsPlaying_orig();
+  void CDStartup();
+  void CDStartup_orig();
+  void CDShutdown();
+  void CDShutdown_orig();
+  void CDPlay(int track_number, signed int a3, int a4, int a5);
+  void CDStop();
+  void CDSetVolume(int, int);
+  void CDSetVolume_orig(int, int);
+  int ConvertVolume(int, int);
+  int ConvertVolume_orig(int, int);
+  void ServiceSound();
+};
 
 #pragma pack(pop)
 
