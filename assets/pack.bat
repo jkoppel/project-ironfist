beb4353dc8d0a0784d8dd46b3d481cd871079a1f
Sergei Nikitin <kertwaii@gmail.com>
2017-04-05 15:56:26 +0600

    Updated packing script

--- assets/pack.bat
+++ assets/pack.bat
@@ -1,3 +1,9 @@
-..\tools\dist\agg\H2AggPack.exe ironfist.agg agg
-if not exist "..\build" mkdir ..\build
-move ironfist.agg ..\build\ironfist.agg
+@echo off 
+for /d %%i in (agg/*.cadres) do call "../tools/dist/icn/H2IcnPack_nodialog.exe" agg/%%i
+for /r %%i in (agg/*.bin.ini) do call "../tools/dist/frm/H2FRMPack_nodialog.exe" "%%i"
+if not exist "ironfist" mkdir ironfist
+move /y agg\*.icn ironfist
+move /y *.bin ironfist
+copy /y agg\*.bmp ironfist
+..\tools\dist\agg\H2AggPack_nodialog.exe ..\build\ironfist.agg ironfist
+RD /S /Q ironfist
