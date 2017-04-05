c962885660e934d3c70e5649e02788e546c0e59a
Sergei Nikitin <kertwaii@gmail.com>
2017-04-05 16:18:11 +0600

    Updated the packer so it's always run from it's own directory

--- assets/pack.bat
+++ assets/pack.bat
@@ -1,4 +1,5 @@
 @echo off 
+pushd %~dp0
 for /d %%i in (agg/*.cadres) do call "../tools/dist/icn/H2IcnPack_nodialog.exe" agg/%%i
 for /r %%i in (agg/*.bin.ini) do call "../tools/dist/frm/H2FRMPack_nodialog.exe" "%%i"
 if not exist "ironfist" mkdir ironfist
@@ -7,3 +8,4 @@ move /y *.bin ironfist
 copy /y agg\*.bmp ironfist
 ..\tools\dist\agg\H2AggPack_nodialog.exe ..\build\ironfist.agg ironfist
 RD /S /Q ironfist
+popd
