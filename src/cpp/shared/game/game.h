6d4ecaa504de32768d544d7a5b28f7ed12b99ac4
email <darien.sokolov@yahoo.com>
2017-05-21 02:39:30 -0500

    unk_4F1E90 changed to giPlayerInitialResourcesAI

--- src/cpp/shared/game/game.h
+++ src/cpp/shared/game/game.h
@@ -228,6 +228,7 @@ extern __int16 giUltimateArtifactRadius;
 
 extern char gpcBeforeGSpellLimits[];
 extern int giPlayerInitialResourcesHuman[NUM_DIFFICULTIES][NUM_RESOURCES];
+extern int giPlayerInitialResourcesAI[NUM_DIFFICULTIES][NUM_RESOURCES];
 
 #pragma pack(pop)
 
