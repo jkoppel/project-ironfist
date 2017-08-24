60f2c78c337340b67561abfe0fc16c6b1eab86af
cometfractals <klondike@uymail.com>
2017-08-24 12:35:49 -0400

    Update scripting.cpp

--- src/cpp/shared/scripting/scripting.cpp
+++ src/cpp/shared/scripting/scripting.cpp
@@ -820,6 +820,14 @@ int l_getguildspell(lua_State *L) {
 	return 1;
 }
 
+int l_grantspellscroll(lua_State *L) {
+	hero* hro = (hero*)GetPointerFromLuaClassTable(L, StackIndexOfArg(1, 2));	
+	int sp = (int)luaL_checknumber(L, 2);	
+	GiveArtifact(hro, ARTIFACT_SPELL_SCROLL, 1, sp);
+	return 0;
+
+}
+
 void set_lua_globals(lua_State *L) {
   lua_register(L, "MessageBox", l_msgbox);
   lua_register(L, "AdvancedMessageBox", l_AdvancedMessageBox);
