eaed7e8d76d3a967c0b0e4a7432176c6f4f2f194
Sergei Nikitin <kertwaii@gmail.com>
2017-08-31 08:35:36 +0600

    Force only walk/only jumpattack

--- src/cpp/shared/combat/army.cpp
+++ src/cpp/shared/combat/army.cpp
@@ -800,7 +800,7 @@ int army::WalkTo(int hex) {
       int dir = *((BYTE *)&gpSearchArray->field_2418 + hexIdxb);
       int destHex = this->GetAdjacentCellIndex(this->occupiedHex, dir);
       if(this->creatureIdx == CREATURE_CYBER_PLASMA_BERSERKER && gIronfistExtra.combat.stack.abilityCounter[this]) {
-        if(gMoveAttack && hexIdxb < 3) { // less than 3 hexes from enemy
+        if(hexIdxb == gpSearchArray->field_8 - 1 && gMoveAttack && hexIdxb < 4) { // less than 4 hexes from enemy on the first step
           // find last hex
           for(int h = hexIdxb; h >= 0; --h) {
             dir = *((BYTE *)&gpSearchArray->field_2418 + h);
@@ -867,7 +867,7 @@ int army::AttackTo(int targetHex) {
         int dir = *((BYTE *)&gpSearchArray->field_2418 + i);
         int destHex = this->GetAdjacentCellIndex(this->occupiedHex, dir);
         if(this->creatureIdx == CREATURE_CYBER_PLASMA_BERSERKER && gIronfistExtra.combat.stack.abilityCounter[this]) {
-          if(gMoveAttack && i < 4) { // less than 3 hexes from enemy
+          if(i == gpSearchArray->field_8 - 1 && gMoveAttack && i < 5) { // less than 4 hexes from enemy on the first step
             // find last hex
             for(int h = i; h >= 1; --h) {
               dir = *((BYTE *)&gpSearchArray->field_2418 + h);
