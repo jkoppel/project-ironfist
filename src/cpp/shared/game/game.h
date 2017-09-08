30a7db026fa20100306875666c29e0b6c9695444
email <darien.sokolov@yahoo.com>
2017-09-08 01:47:49 -0500

    Imported/Globalized creatureTypesAndQuantities
    Moved this structure from within a function and made it global.

--- src/cpp/shared/game/game.h
+++ src/cpp/shared/game/game.h
@@ -236,6 +236,8 @@ extern char gpcBeforeGSpellLimits[];
 extern int giPlayerInitialResourcesHuman[NUM_DIFFICULTIES][NUM_RESOURCES];
 extern int giPlayerInitialResourcesAI[NUM_DIFFICULTIES][NUM_RESOURCES];
 
+extern __int16 creatureTypesAndQuantities[NUM_FACTIONS][3][3];
+
 extern signed char gHeroSkillBonus[NUM_FACTIONS][2][4];
 
 #pragma pack(pop)
