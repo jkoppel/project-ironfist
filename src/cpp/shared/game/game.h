a81746018f3b114abd49046a36e33fd8bc079c19
email <darien.sokolov@yahoo.com>
2017-09-24 22:49:47 -0500

    Revert "Added declarations for RandomScan() and heroMapExtra"
    This reverts commit 7ddb72a189b9d6e3ca85c8dd2ac611cda71c8a73.

--- src/cpp/shared/game/game.h
+++ src/cpp/shared/game/game.h
@@ -210,7 +210,6 @@ public:
 
   void SetRandomHeroArmies(int heroIdx, int isAI);
   void GiveTroopsToNeutralTown(int castleIdx);
-  int RandomScan(signed char*, int, int, int, signed char);
 
 private:
   void PropagateVision();
