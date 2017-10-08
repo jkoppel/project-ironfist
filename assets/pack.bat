71987805ea40726138f004ada3294f993a4467ee
James Koppel <jamesbkoppel@yahoo.com>
2017-10-08 19:03:10 -0400

    Positioned high scores button

--- assets/pack.bat
+++ assets/pack.bat
@@ -5,6 +5,7 @@ for /r %%i in (agg/*.bin.ini) do call "../tools/dist/frm/H2FRMPack_nodialog.exe"
 if not exist "ironfist" mkdir ironfist
 move /y agg\*.icn ironfist
 move /y agg\*FRM.bin ironfist
+copy /y agg\*.bin ironfist
 copy /y agg\*.bmp ironfist
 copy /y agg\*.82M ironfist
 ..\tools\dist\agg\H2AggPack_nodialog.exe ..\build\ironfist.agg ironfist
