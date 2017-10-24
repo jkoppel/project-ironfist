316900878ec59f04decb8f1b4ed2d076f4701b29
cometfractals <klondike@uymail.com>
2017-10-24 08:56:23 -0400

    Function Declaration CancelShareVision

--- src/cpp/shared/game/game.h
+++ src/cpp/shared/game/game.h
@@ -187,6 +187,7 @@ public:
 
     void ResetIronfistGameState();
     void ShareVision(int sourcePlayer, int destPlayer);
+    void CancelShareVision(int sourcePlayer, int destPlayer);	
 
     void MakeAllWaterVisible(int player);
     void MakeAllWaterVisible_orig(int player);
