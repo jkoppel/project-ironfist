4ee2727652e8e65eb479e8920083454859fcc12f
Darien Sokolov <darien.sokolov@yahoo.com>
2018-01-28 02:20:10 -0600

    Cleaned formatting for decompilation

--- src/raw_decompiled/HEROES2W.c
+++ src/raw_decompiled/HEROES2W.c
@@ -76811,26 +76811,17 @@ void __thiscall hero::Deallocate(hero *this, int a2)
 int __stdcall hero::GetExperience(signed int level)
 {
   int result; // eax@2
-  signed int v2; // [sp+18h] [bp-Ch]@3
-  int exp; // [sp+1Ch] [bp-8h]@3
-  signed int v4; // [sp+20h] [bp-4h]@3
+  signed int experience; // [sp+20h] [bp-4h]@3
 
-  if ( level > 12 )
-  {
-    v2 = 13;
-    v4 = (signed __int64)((double)((signed int)experienceForLevelTable[12] - (signed int)experienceForLevelTable[11])
-                        * 1.2);
-    exp = v4 + experienceForLevelTable[12];
-    while ( v2 < level )
-    {
-      v4 = (signed __int64)((double)v4 * 1.2);
-      exp += v4;
-      ++v2;
+  if (level > 12) {    
+    experience = (experienceForLevelTable[12] - experienceForLevelTable[11]) * 1.2;
+    result = experience + experienceForLevelTable[12];
+    for (int i = 13; i < level; ++i) {
+      experience *=  1.2;
+      result += experience;      
     }
-    result = exp;
   }
-  else
-  {
+  else {
     result = experienceForLevelTable[level];
   }
   return result;
