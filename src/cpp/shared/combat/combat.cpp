2862d72d8e382e32ad7e81c6cae1bfdd6a240257
Sergei Nikitin <kertwaii@gmail.com>
2018-07-30 13:37:18 +0600

    Refactoring for SetCombatDirections

--- src/cpp/shared/combat/combat.cpp
+++ src/cpp/shared/combat/combat.cpp
@@ -584,171 +584,165 @@ std::vector<COORD> MakeCatapultArc(int numPoints, bool lefttoright, float fromX,
   return points;
 }
 
+// Decides how a certain hex can be interacted from each direction by current creature
 void combatManager::SetCombatDirections(int hexIdx) {
-  int v4; // [sp+10h] [bp-78h]@2
-  int v5[8]; // [sp+14h] [bp-74h]@31
-  int v6; // [sp+34h] [bp-54h]@108
-  army *thisb; // [sp+38h] [bp-50h]@2
-  int v8; // [sp+3Ch] [bp-4Ch]@108
-  char v10[8]; // [sp+44h] [bp-44h]@53
-  int v12; // [sp+4Ch] [bp-3Ch]@103
-  int v13; // [sp+50h] [bp-38h]@2
-  int v14; // [sp+54h] [bp-34h]@74
-  army *v15; // [sp+58h] [bp-30h]@2
-  int a2[8]; // [sp+5Ch] [bp-2Ch]@9
-  int v17; // [sp+7Ch] [bp-Ch]@75
-  char v18[8]; // [sp+80h] [bp-8h]@47
+  if(this->field_F2B3)
+    return;
 
-  if(!this->field_F2B3) {
-    thisb = &this->creatures[this->otherCurrentSideThing][this->someSortOfStackIdx];
-    v4 = thisb->targetOwner;
-    v13 = thisb->targetStackIdx;
-    thisb->targetOwner = -1;
-    thisb->targetStackIdx = -1;
-    v15 = &this->creatures[v4][v13];
-    for(int i = 0; i < 8; ++i) {
-      if(i != 6 && i != 7) {
-        a2[i] = this->hexNeighbors[hexIdx][i];
-      } else if(thisb->creature.creature_flags & TWO_HEXER) {
-        if(thisb->facingRight == 1) {
-          if(i == 6)
-            a2[6] = this->hexNeighbors[hexIdx][5];
-          if(i == 7)
-            a2[7] = this->hexNeighbors[hexIdx][3];
-        } else {
-          if(i == 6)
-            a2[6] = this->hexNeighbors[hexIdx][0];
-          if(i == 7)
-            a2[7] = this->hexNeighbors[hexIdx][2];
-        }
-      } else {
-        a2[i] = -1;
-      }
-      if(thisb->creature.creature_flags & TWO_HEXER && a2[i] != -1) {
-        if(thisb->facingRight == 1) {
-          if(i == 5 || i == 4 || i == 3) {
-            if(a2[i] % 13 == 1)
-              a2[i] = -1;
-            else
-              --a2[i];
-          }
-          if(a2[i] % 13 == 11)
-            v5[i] = -1;
-          else
-            v5[i] = a2[i] + 1;
-        } else {
-          if(!i || i == 1 || i == 2) {
-            if(a2[i] % 13 == 11)
-              a2[i] = -1;
-            else
-              ++a2[i];
-          }
-          if(a2[i] % 13 == 1)
-            v5[i] = -1;
-          else
-            v5[i] = a2[i] - 1;
-        }
-      } else {
-        v5[i] = -2;
-      }
-      v18[i] = this->ValidHexToStandOn(a2[i]) && this->ValidHexToStandOn(v5[i]);
-    }
-    if(thisb->creature.creature_flags & FLYER) {
-      for(int i = 0; i < 8; ++i)
-        v10[i] = v18[i];
-    } else {
-      for(int i = 0; i < 8; ++i) {
-        if(v18[i])
-          if((CreatureHasAttribute(thisb->creatureIdx, CHARGER) && thisb->GetStraightLineDistanceToHex(a2[i]) <= thisb->creature.speed && thisb->TargetOnStraightLine(a2[i]) && thisb->TargetOnStraightLine(hexIdx) && thisb->ValidFlight(a2[i], 0))) {
-            v10[i] = a2[i] = 1;
-          }
-          else
-            v10[i] = a2[i] == thisb->occupiedHex || thisb->ValidPath(a2[i], 1);
-        else
-          v10[i] = 0;
-      }
-    }
-    *(DWORD *)&this->_15[64] = 0;
-    for(int i = 0; i < 8; ++i) {
-      if(v10[i])
-        ++*(DWORD *)&this->_15[64];
-    }
-    if(!*(DWORD *)&this->_15[64])
-      v10[6] = 1;
-    memset(&this->_15[36], 0xFFu, 0x18u);
-    for(int i = 0; i < 8; ++i) {
-      v14 = i;
-      if(i >= 6) {
+  int v5[8]; // [sp+14h] [bp-74h]@31
+  char v10[8]; // [sp+44h] [bp-44h]@53
+  int a2[8]; // [sp+5Ch] [bp-2Ch]@9
+  char v18[8]; // [sp+80h] [bp-8h]@47
+  army* attacker = &this->creatures[this->otherCurrentSideThing][this->someSortOfStackIdx];
+  int targetOwner = attacker->targetOwner;
+  int targetStackIdx = attacker->targetStackIdx;
+  attacker->targetOwner = -1;
+  attacker->targetStackIdx = -1;
+  army* target = &this->creatures[targetOwner][targetStackIdx];
+  for(int i = 0; i < 8; ++i) {
+    if(i != 6 && i != 7) {
+      a2[i] = this->hexNeighbors[hexIdx][i];
+    } else if(attacker->creature.creature_flags & TWO_HEXER) {
+      if(attacker->facingRight == 1) {
         if(i == 6)
-          v17 = 7;
-        else
-          v17 = 6;
+          a2[6] = this->hexNeighbors[hexIdx][5];
+        if(i == 7)
+          a2[7] = this->hexNeighbors[hexIdx][3];
       } else {
-        v17 = (i + 3) % 6;
+        if(i == 6)
+          a2[6] = this->hexNeighbors[hexIdx][0];
+        if(i == 7)
+          a2[7] = this->hexNeighbors[hexIdx][2];
       }
-      if(v10[v17]) {
-        if(v15->creature.creature_flags & TWO_HEXER) {
-          if(i || this->combatGrid[hexIdx - 1].unitOwner != v4 || this->combatGrid[hexIdx - 1].stackIdx != v13) {
-            if(i != 5
-              || this->combatGrid[hexIdx + 1].unitOwner != v4
-              || this->combatGrid[hexIdx + 1].stackIdx != v13) {
-              if(i != 2
-                || this->combatGrid[hexIdx - 1].unitOwner != v4
-                || this->combatGrid[hexIdx - 1].stackIdx != v13) {
-                if(i == 3
-                  && this->combatGrid[hexIdx + 1].unitOwner == v4
-                  && this->combatGrid[hexIdx + 1].stackIdx == v13)
-                  v14 = 7;
-              } else {
+    } else {
+      a2[i] = -1;
+    }
+    if(attacker->creature.creature_flags & TWO_HEXER && a2[i] != -1) {
+      if(attacker->facingRight == 1) {
+        if(i == 5 || i == 4 || i == 3) {
+          if(a2[i] % 13 == 1)
+            a2[i] = -1;
+          else
+            --a2[i];
+        }
+        if(a2[i] % 13 == 11)
+          v5[i] = -1;
+        else
+          v5[i] = a2[i] + 1;
+      } else {
+        if(!i || i == 1 || i == 2) {
+          if(a2[i] % 13 == 11)
+            a2[i] = -1;
+          else
+            ++a2[i];
+        }
+        if(a2[i] % 13 == 1)
+          v5[i] = -1;
+        else
+          v5[i] = a2[i] - 1;
+      }
+    } else {
+      v5[i] = -2;
+    }
+    v18[i] = this->ValidHexToStandOn(a2[i]) && this->ValidHexToStandOn(v5[i]);
+  }
+  if(attacker->creature.creature_flags & FLYER) {
+    for(int i = 0; i < 8; ++i)
+      v10[i] = v18[i];
+  } else {
+    for(int i = 0; i < 8; ++i) {
+      if(v18[i])
+        if((CreatureHasAttribute(attacker->creatureIdx, CHARGER) && attacker->GetStraightLineDistanceToHex(a2[i]) <= attacker->creature.speed && attacker->TargetOnStraightLine(a2[i]) && attacker->TargetOnStraightLine(hexIdx) && attacker->ValidFlight(a2[i], 0))) {
+          v10[i] = a2[i] = 1;
+        }
+        else
+          v10[i] = a2[i] == attacker->occupiedHex || attacker->ValidPath(a2[i], 1);
+      else
+        v10[i] = 0;
+    }
+  }
+  *(DWORD *)&this->_15[64] = 0;
+  for(int i = 0; i < 8; ++i) {
+    if(v10[i])
+      ++*(DWORD *)&this->_15[64];
+  }
+  if(!*(DWORD *)&this->_15[64])
+    v10[6] = 1;
+  memset(&this->_15[36], 0xFFu, 0x18u);
+  
+  int v17;
+  for(int i = 0; i < 8; ++i) {
+    int v14 = i;
+    if(i >= 6) {
+      if(i == 6)
+        v17 = 7;
+      else
+        v17 = 6;
+    } else {
+      v17 = (i + 3) % 6;
+    }
+    if(v10[v17]) {
+      if(target->creature.creature_flags & TWO_HEXER) {
+        if(i || this->combatGrid[hexIdx - 1].unitOwner != targetOwner || this->combatGrid[hexIdx - 1].stackIdx != targetStackIdx) {
+          if(i != 5
+            || this->combatGrid[hexIdx + 1].unitOwner != targetOwner
+            || this->combatGrid[hexIdx + 1].stackIdx != targetStackIdx) {
+            if(i != 2
+              || this->combatGrid[hexIdx - 1].unitOwner != targetOwner
+              || this->combatGrid[hexIdx - 1].stackIdx != targetStackIdx) {
+              if(i == 3
+                && this->combatGrid[hexIdx + 1].unitOwner == targetOwner
+                && this->combatGrid[hexIdx + 1].stackIdx == targetStackIdx)
                 v14 = 7;
-              }
             } else {
-              v14 = 6;
+              v14 = 7;
             }
           } else {
             v14 = 6;
           }
-        }
-        if(i >= 6) {
-          if(i == 6) {
-            this->_15[47] = v14;
-            this->_15[48] = v14;
-            this->_15[49] = v14;
-          } else {
-            this->_15[36] = v14;
-            this->_15[37] = v14;
-            this->_15[59] = v14;
-          }
         } else {
-          memset(&this->_15[4 * v17 + 36], v14, 4u);
+          v14 = 6;
         }
+      }
+      if(i >= 6) {
+        if(i == 6) {
+          this->_15[47] = v14;
+          this->_15[48] = v14;
+          this->_15[49] = v14;
+        } else {
+          this->_15[36] = v14;
+          this->_15[37] = v14;
+          this->_15[59] = v14;
+        }
+      } else {
+        memset(&this->_15[4 * v17 + 36], v14, 4u);
       }
     }
-    v12 = 24;
-    while(v12 > 0) {
-      for(int i = 0; i < 24; ++i) {
-        if(this->_15[i + 36] == -1) {
-          v6 = (i + 1) % 24;
-          v8 = (i + 23) % 24;
-          if(this->_15[(i + 1) % 24 + 36] < 0 || this->_15[v6 + 36] > 7) {
-            if(this->_15[v8 + 36] >= 0 && this->_15[v8 + 36] <= 7)
-              this->_15[i + 36] = this->_15[v8 + 36] + 10;
-          } else {
-            this->_15[i + 36] = this->_15[v6 + 36] + 10;
-          }
-        }
-      }
-      v12 = 0;
-      for(int i = 0; i < 24; ++i) {
-        if(this->_15[i + 36] < 10) {
-          if(this->_15[i + 36] == -1)
-            ++v12;
-        } else {
-          this->_15[i + 36] -= 10;
-        }
-      }
-    }
-    thisb->targetOwner = v4;
-    thisb->targetStackIdx = v13;
   }
+  int v12 = 24;
+  while(v12 > 0) {
+    for(int i = 0; i < 24; ++i) {
+      if(this->_15[i + 36] == -1) {
+        int v6 = (i + 1) % 24;
+        int v8 = (i + 23) % 24;
+        if(this->_15[(i + 1) % 24 + 36] < 0 || this->_15[v6 + 36] > 7) {
+          if(this->_15[v8 + 36] >= 0 && this->_15[v8 + 36] <= 7)
+            this->_15[i + 36] = this->_15[v8 + 36] + 10;
+        } else {
+          this->_15[i + 36] = this->_15[v6 + 36] + 10;
+        }
+      }
+    }
+    v12 = 0;
+    for(int i = 0; i < 24; ++i) {
+      if(this->_15[i + 36] < 10) {
+        if(this->_15[i + 36] == -1)
+          ++v12;
+      } else {
+        this->_15[i + 36] -= 10;
+      }
+    }
+  }
+  attacker->targetOwner = targetOwner;
+  attacker->targetStackIdx = targetStackIdx;
 }
