5c65c689816ace88ba9b56b34dedee4f3437605e
Darien Sokolov <darien.sokolov@yahoo.com>
2018-10-09 14:38:37 -0500

    Added overload for char*

--- src/cpp/shared/scripting/callback.h
+++ src/cpp/shared/scripting/callback.h
@@ -16,6 +16,7 @@ extern "C" {
 void ironfist_lua_push(int arg);
 void ironfist_lua_push(bool arg);
 void ironfist_lua_push(void *arg);
+void ironfist_lua_push(char *arg);
 void ironfist_lua_push(std::string arg);
 void ironfist_lua_pushmulti();
 
