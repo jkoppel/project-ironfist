ccaaf716bd115f8edf67db7b94311c684f2709ab
Michael Kristofik <kristo605@gmail.com>
2018-10-16 23:40:39 -0500

    Added Cyborg town prefix.

--- src/cpp/shared/town/town.cpp
+++ src/cpp/shared/town/town.cpp
@@ -49,6 +49,12 @@ extern char *gNeutralBuildingNames[];
 extern char *gDwellingNames[][NUM_DWELLINGS];
 extern char *gBuildingInfoSpecial[MAX_FACTIONS] = { 0 };
 
+extern char *gTownPrefixNames[MAX_FACTIONS] = {
+  "twnk", "twnb", "twns", "twnw", "twnz", "twnn",
+  "", "", "", "", "", "",
+  "twnc"
+};
+
 std::vector<std::string> objectNames = {
   "mage",
   "thie",
