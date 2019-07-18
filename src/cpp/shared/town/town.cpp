d6547858eb11047f18cc6e3a704fdb2b3fa879b5
Sergei Nikitin <kertwaii@gmail.com>
2019-07-18 09:22:46 +0600

    Use -1 instead of faction image offset for certain town screen actions because it will be converted into -1 in NormalDialog anyway

--- src/cpp/shared/town/town.cpp
+++ src/cpp/shared/town/town.cpp
@@ -836,16 +836,10 @@ int townManager::Main(tag_message &evt) {
           if(rightClick) {
           LABEL_127:
             char *buildInfo = GetBuildingInfo(this->castle->factionID, evt.yCoordOrFieldID, 1);
-            if(this->castle->factionID == FACTION_CYBORG)
-              NormalDialog(buildInfo, 4, -1, -1, 19, evt.yCoordOrFieldID, -1, 0, -1, 0);
-            else
-              NormalDialog(buildInfo, 4, -1, -1, this->castle->factionID + 19, evt.yCoordOrFieldID, -1, 0, -1, 0);
+              NormalDialog(buildInfo, 4, -1, -1, -1, evt.yCoordOrFieldID, -1, 0, -1, 0);
           } else {
             char *buildInfo = GetBuildingInfo(this->castle->factionID, evt.yCoordOrFieldID, 1);
-            if(this->castle->factionID == FACTION_CYBORG)
-              NormalDialog(buildInfo, 1, -1, -1, 19, evt.yCoordOrFieldID, -1, 0, -1, 0);
-            else
-              NormalDialog(buildInfo, 1, -1, -1, this->castle->factionID + 19, evt.yCoordOrFieldID, -1, 0, -1, 0);
+              NormalDialog(buildInfo, 1, -1, -1, -1, evt.yCoordOrFieldID, -1, 0, -1, 0);
           }
           goto LABEL_192;
         }
