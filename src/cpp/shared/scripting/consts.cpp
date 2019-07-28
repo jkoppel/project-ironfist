8af44a3fc4727940e7718881d6d612d54e290c40
James Koppel <jamesbkoppel@yahoo.com>
2019-07-28 00:07:09 -0400

    Making AdvancedMessageBox return an answer

--- src/cpp/shared/scripting/consts.cpp
+++ src/cpp/shared/scripting/consts.cpp
@@ -38,10 +38,14 @@ namespace {
 /******************************* GUI *****************************************************/
 
 void set_dialog_consts(lua_State *L) {
-	lua_setconst(L, "DIALOG_YES_NO", DIALOG_YES_NO);
-	lua_setconst(L, "DIALOG_OKAY", DIALOG_OKAY);
-	lua_setconst(L, "DIALOG_OR", DIALOG_OR);
-	lua_setconst(L, "DIALOG_EMPTY", DIALOG_EMPTY);
+  lua_setconst(L, "DIALOG_OKAY", DIALOG_OKAY);
+  lua_setconst(L, "DIALOG_YES_NO", DIALOG_YES_NO);
+  lua_setconst(L, "DIALOG_CANCEL", DIALOG_CANCEL);
+  lua_setconst(L, "DIALOG_RIGHT_CLICK", DIALOG_RIGHT_CLICK);
+  lua_setconst(L, "DIALOG_LEARN_CHOICE", DIALOG_LEARN_CHOICE);
+
+  lua_setconst(L, "DIALOG_OR", DIALOG_OR);
+  lua_setconst(L, "DIALOG_EMPTY", DIALOG_EMPTY);
 }
 
 
--- src/cpp/shared/scripting/funcs.cpp
+++ src/cpp/shared/scripting/funcs.cpp
@@ -77,9 +77,11 @@ static int l_msgBox(lua_State *L) {
   return 0;
 }
 
+extern int gbFunctionComplete;
+
 static int l_AdvancedMessageBox(lua_State *L) {
   const char* msg = luaL_checkstring(L, 1);
-  int yesno = luaL_checkinteger(L, 2);
+  int dialogType = luaL_checkinteger(L, 2);
   int horizontal = luaL_checkinteger(L, 3);
   int vertical = luaL_checknumber(L, 4);
   int img1type = luaL_checknumber(L, 5);
@@ -88,8 +90,21 @@ static int l_AdvancedMessageBox(lua_State *L) {
   int img2arg = luaL_checknumber(L, 8);
   int writeOr = luaL_checknumber(L, 9);
   int a10 = luaL_checknumber(L, 10);
-  NormalDialog((char*)msg, (int)yesno, (int)horizontal, (int)vertical, (int)img1type, (int)img1arg, (int)img2type, (int)img2arg, (int)writeOr, (int)a10);
-  return 0;
+  
+  std::string msgCopy = msg;
+  NormalDialog(&msgCopy[0], dialogType, horizontal, vertical, img1type, img1arg, img2type, img2arg, writeOr, a10);
+ 
+  if (dialogType == DIALOG_LEARN_CHOICE) { // learn dialog
+    lua_pushboolean(L, gpWindowManager->buttonPressedCode == BUTTON_CODE_LEARN_LEFT);
+  } else if (dialogType == DIALOG_CANCEL_ALT) {
+    lua_pushboolean(L, gbFunctionComplete);
+  } else if (dialogType == DIALOG_YES_NO) {
+    lua_pushboolean(L, gpWindowManager->buttonPressedCode == BUTTON_CODE_OKAY);
+  } else {
+    lua_pushboolean(L, true);
+  }
+  
+  return 1;
 }
 
 static int l_questionBox(lua_State *L) {
