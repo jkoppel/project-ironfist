93610867cc2a4801b1f5afeccc8605b13f9d440f
Sergei Nikitin <kertwaii@gmail.com>
2020-06-18 15:39:35 +0600

    Show images for creatures

--- maps/MODULES/locations_1.lua
+++ maps/MODULES/locations_1.lua
@@ -1,4 +1,5 @@
 local locations = {}
+AdvMessageBox = require "scripts.modules.AdvMessageBox"
 
 function locations.CyborgUpgradeLocation()
 	h = GetCurrentHero()
@@ -6,8 +7,8 @@ function locations.CyborgUpgradeLocation()
 	-- someone else
 	pikemen = tonumber(GetCreatureAmount(h, CREATURE_PIKEMAN)) or 0
 	if (goblins + pikemen) == 0 then
-		AdvancedMessageBox("{Hill Fort}\n\nAn unusual alliance of Cyborgs to train (upgrade) Pikemen and Goblins.  Unfortunately, you have none with you.",
-	  1, -1, -1, -1, -1, -1, -1, -1, 0)
+		AdvMessageBox.DoubleImage("{Hill Fort}\n\nAn unusual alliance of Cyborgs to train (upgrade) Pikemen and Goblins.  Unfortunately, you have none with you.",
+	  1, IMAGE_GROUP_UNIT, CREATURE_PIKEMAN, IMAGE_GROUP_UNIT, CREATURE_GOBLIN)
 	else
 		if goblins > 0 then 
 			TakeTroop(h, CREATURE_GOBLIN, goblins)
@@ -20,11 +21,11 @@ function locations.CyborgUpgradeLocation()
 		end
 		PlaySoundEffect('EXPERNCE')
 		if goblins > 0 and pikemen > 0 then
-			AdvancedMessageBox("{Hill Fort}\n\nAll of your Pikemen and Goblins have been upgraded to Cyborg Plasma Lancers and Cyborg Kobold Spearmen", 1, -1, -1, -1, -1, -1, -1, -1, 0)
+			AdvMessageBox.DoubleImage("{Hill Fort}\n\nAll of your Pikemen and Goblins have been upgraded to Cyborg Plasma Lancers and Cyborg Kobold Spearmen", 1, IMAGE_GROUP_UNIT, CREATURE_CYBER_PLASMA_LANCER, IMAGE_GROUP_UNIT, CREATURE_CYBER_KOBOLD_SPEARMAN)
 		elseif goblins > 0 then
-			AdvancedMessageBox("{Hill Fort}\n\nAll of your Goblins have been upgraded to Cyborg Kobold Spearmen", 1, -1, -1, -1, -1, -1, -1, -1, 0)
+			AdvMessageBox.SingleImage("{Hill Fort}\n\nAll of your Goblins have been upgraded to Cyborg Kobold Spearmen", 1, IMAGE_GROUP_UNIT, CREATURE_CYBER_KOBOLD_SPEARMAN)
 		elseif pikemen > 0 then
-			AdvancedMessageBox("{Hill Fort}\n\nAll of your Pikemen have been upgraded to Cyborg Plasma Lancers", 1, -1, -1, -1, -1, -1, -1, -1, 0)
+			AdvMessageBox.SingleImage("{Hill Fort}\n\nAll of your Pikemen have been upgraded to Cyborg Plasma Lancers", 1, IMAGE_GROUP_UNIT, CREATURE_CYBER_PLASMA_LANCER)
 		end
 	end
 	return true
